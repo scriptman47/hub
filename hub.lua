7bcf017d0b8b71d325d592eda0352c516016793c
scriptman47 <chillpal26@gmail.com>
2025-03-29 04:38:44 +0800

    Delete hub.lua

--- hub.lua
+++
@@ -1,9 +0,0 @@
-if game.PlaceId == 258258996 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/m.lua"))()
-elseif game.PlaceId == 432041175 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/bd.lua"))()
-elseif game.Workspace:FindFirstChild("Tycoons") then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/tm.lua"))()
-elseif game.PlaceId == 15055025587 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/egg.lua"))()
-end
