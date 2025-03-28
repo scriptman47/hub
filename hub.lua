fecc012b272916d1fda6c9a2d4f516084ba6a9a8
scriptman47 <chillpal26@gmail.com>
2025-03-29 04:28:06 +0800

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,9 +1,9 @@
 if game.PlaceId == 258258996 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/hub/m.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/m.lua"))()
 elseif game.PlaceId == 432041175 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/hub/bd.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/bd.lua"))()
 elseif game.Workspace:FindFirstChild("Tycoons") then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/hub/tm.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/tm.lua"))()
 elseif game.PlaceId == 15055025587 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/hub/egg.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/egg.lua"))()
 end
