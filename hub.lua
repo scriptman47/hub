d2f415e626f4e83081d32286eb6e75ac80330572
scriptman47 <chillpal26@gmail.com>
2025-03-29 04:16:17 +0800

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -36,11 +36,11 @@ local capList = {
     [15] = 1e+30
 }
 if game.PlaceId == 258258996 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/zqx257/hub/refs/heads/main/hub/m.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/hub/m.lua"))()
 elseif game.PlaceId == 432041175 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/zqx257/hub/refs/heads/main/hub/bd.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/hub/bd.lua"))()
 elseif game.Workspace:FindFirstChild("Tycoons") then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/zqx257/hub/refs/heads/main/hub/tm.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/hub/tm.lua"))()
 elseif game.PlaceId == 15055025587 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/zqx257/hub/refs/heads/main/hub/egg.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/hub/egg.lua"))()
 end
