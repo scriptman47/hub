18192734fd47c3ffb59b3f58ff2dcdaa16bca93e
scriptman47 <chillpal26@gmail.com>
2025-03-29 04:26:03 +0800

    Add files via upload

---
+++ hub.lua
@@ -0,0 +1,9 @@
+if game.PlaceId == 258258996 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/hub/m.lua"))()
+elseif game.PlaceId == 432041175 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/hub/bd.lua"))()
+elseif game.Workspace:FindFirstChild("Tycoons") then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/hub/tm.lua"))()
+elseif game.PlaceId == 15055025587 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptman47/hub/refs/heads/main/hub/egg.lua"))()
+end
